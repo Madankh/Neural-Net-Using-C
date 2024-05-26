7754b70940542541eac27c104b296d4989e02656
madankh <khadkasuman0000@gmail.com>
2024-05-26 17:06:14 +0545

    got a error on expf

--- gates.c
+++ gates.c
@@ -1,50 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+float sigmoidf(float x) {
+    return 1.0f / (1.0f + expf(-x));
+}
 
 // OR -- gate
 float train[][3] = {
-    {0,0,0},
-    {1,0,1},
-    {0,1, 1},
-    {1,1,1},
+    {0, 0, 0},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 1},
 };
 
-#define train_count sizeof(train)/sizeof(train[0])
+#define train_count (sizeof(train) / sizeof(train[0]))
 
-float cost(float w1, float w2){
+float cost(float w1, float w2) {
     float result = 0.0f;
-    for(size_t i = 0; i<train_count; ++i){
+    for (size_t i = 0; i < train_count; ++i) {
         float x1 = train[i][0];
-        float x2 = train[i][0];
-        float y = x1*w1+ x2+w2;
+        float x2 = train[i][1];  // Corrected index
+        float y = sigmoidf(x1 * w1 + x2 * w2);  // Corrected formula
         float d = y - train[i][2];
-        result +=d*d;
+        result += d * d;
     }
-    result /=train_count;
+    result /= train_count;
     return result;
-};
+}
 
-float rand_float(void){
-    return (float) rand() / (float) RAND_MAX;
-};
+float rand_float(void) {
+    return (float)rand() / (float)RAND_MAX;
+}
 
+int main(void) {
 
-int main(void){
-    srand(69);
+    for (float x = -10.f; x <= 10.f; x += 1.f) {
+        printf("%f => %f\n", x, sigmoidf(x));
+    }
+    return 0;
+    srand(69);  // Moved srand before generating random numbers
     float w1 = rand_float();
     float w2 = rand_float();
 
     float eps = 1e-3;
     float rate = 1e-3;
 
-    for(size_t i =0; i<2000; i++){
-        float c = cost(w1,w2);
-        printf("W1 = %f, W2 = %f, c=%f\n", w1,w2,c);
-        float dw1 = (cost(w1 + eps, w2) - c)/eps;
-        float dw2 = (cost(w1, w2+eps) - c)/eps;
-        w1 -= rate*dw1;
-        w2 -= rate*dw2;
+    for (size_t i = 0; i < 2000; i++) {
+        float c = cost(w1, w2);
+        printf("W1 = %f, W2 = %f, c=%f\n", w1, w2, c);
+        float dw1 = (cost(w1 + eps, w2) - c) / eps;
+        float dw2 = (cost(w1, w2 + eps) - c) / eps;
+        w1 -= rate * dw1;
+        w2 -= rate * dw2;
     }
-    printf("W1 = %f , W2 = %f \n", w1, w2);
+    printf("W1 = %f, W2 = %f\n", w1, w2);
+
+    return 0;
 }
